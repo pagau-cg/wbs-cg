6a58561fd68051880a1da7859a918fcbdab725e3
pagau-cg <pagauthier@constructiongauthier.com>
2026-05-11 11:40:32 -0400

    Update index.js

--- api/listProjects/index.js
+++ api/listProjects/index.js
@@ -1,10 +1,6 @@
-const { getContainer } = require("../shared/storageHelper");
-
 module.exports = async function (context, req) {
-  try {
-    const container = await getContainer();
-    context.res = { status: 200, body: { ok: true, container: container.containerName } };
-  } catch (err) {
-    context.res = { status: 200, body: { error: err.message, type: err.constructor.name } };
-  }
+  context.res = { 
+    status: 200, 
+    body: { ok: true, msg: "fonction OK" } 
+  };
 };
